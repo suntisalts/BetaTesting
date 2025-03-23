1b1617733aec49ec6e3b74e02e27021a62d2f605
suntisalts <suntisalts@gmail.com>
2025-03-23 18:10:54 +0100

    Update PlayerKick2.lua

--- PlayerKick2.lua
+++ PlayerKick2.lua
@@ -1,1 +1,7 @@
-d
+    local Players = game:GetService("Players")
+    local LocalPlayer = Players.LocalPlayer
+
+    if #Players:GetPlayers() > 1 then
+	    game.Players.LocalPlayer:Kick("Warning: The Button Got Pressed Aleady, If The Button Got Pressed Already It Dosent Work, For Informations: https://discord.gg/wvswdjBnH9. Try Later Again!!")
+        return
+    end
