4ab09b794f8118c9b8065646a2640fb48b498e3d
suntisalts <suntisalts@gmail.com>
2025-03-24 20:10:33 +0100

    Update DragonHarpoon.lua

--- Items/DragonHarpoon.lua
+++ Items/DragonHarpoon.lua
@@ -1,6 +1,4 @@
 workspace.PromptRobuxEvent:InvokeServer(1109792341, "Product")
 
-while true do
-    print("Item Bought: Cookie Wheels.")
-    print("https://discord.gg/Fx2SpRqk68")
-end
+print("Item Bought: Dragon Harpoon.")
+print("https://discord.gg/Fx2SpRqk68")
