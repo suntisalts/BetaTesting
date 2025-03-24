1867b8752f2bc2c6a928b85d03e01e5e5d27652c
suntisalts <suntisalts@gmail.com>
2025-03-24 20:11:10 +0100

    Update CandyHarpoon.lua

--- Items/CandyHarpoon.lua
+++ Items/CandyHarpoon.lua
@@ -1,6 +1,4 @@
 workspace.PromptRobuxEvent:InvokeServer(915766549, "Product")
 
-while true do
-    print("Item Bought: Cookie Wheels.")
-    print("https://discord.gg/Fx2SpRqk68")
-end
+print("Item Bought: Cookie Wheels.")
+print("https://discord.gg/Fx2SpRqk68")
