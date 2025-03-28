34224f0c287e1c29afc511f5634c82b63c273363
suntisalts <suntisalts@gmail.com>
2025-03-28 23:18:14 +0100

    Update AutofarmV1.lua

--- AutofarmV1.lua
+++ AutofarmV1.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))()
+-- Old Loadsting Removed, using this Loadstring now: 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/suntisalts/BetaTesting/refs/heads/main/Autofarm.lua"))()
